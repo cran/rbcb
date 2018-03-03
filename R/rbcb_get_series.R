e7a5076915c3919bd3505051edf0cf3d3f290e37
Wilson Freitas <wilson.freitas@gmail.com>
2018-03-03 10:35:48 +0000

    version 0.1.3

--- R/rbcb_get_series.R
+++ R/rbcb_get_series.R
@@ -4,9 +4,7 @@
 #' @param start_date series initial date. Accepts ISO character formated date and \code{Date}.
 #' @param end_date series final date. Accepts ISO character formated date and \code{Date}.
 #' @param last last items of the series
-#' @param name series name to be used in the returning object
 #' @param as the returning type: data objects (\code{tibble, xts, data.frame, ts}) or \code{text} for raw JSON
-#' @param ts_options options to be passed to \code{ts} function (when \code{as = 'ts'} provided)
 #'
 #' \code{code} argument can be obtained in the SGS system site. In this site searches can be executed in
 #' order to find out the desired series and use the series code in the \code{code} argument.
@@ -17,9 +15,6 @@
 #' download the last \code{N} registers define the \code{last} argument to \code{N}
 #' a positive integer.
 #' Once \code{last} is provided it overrides the arguments \code{start_date} and \code{end_date}.
-#'
-#' If \code{name} argument is set the returning series is properly named, if not the code argument is used.
-#' Note that the code is \code{numeric}.
 #'
 #'
 #' @return
@@ -38,61 +33,113 @@
 #'
 #' @export
 get_series <- function(code, start_date = NULL, end_date = NULL, last = 0,
-                       name = NULL,
-                       as = c('tibble', 'xts', 'ts', 'data.frame', 'text'), ts_options = NULL) {
+                       as = c('tibble', 'xts', 'ts', 'data.frame', 'text')) {
   as <- match.arg(as)
-  url <- series_url(code, start_date, end_date, last)
+  objs = series_obj(code)
+
+  series = lapply(objs, function(x) .get_series(series_url(x, start_date, end_date, last)))
+
+  series = mapply(create_series, series, objs,
+                  MoreArgs = list(as = as),
+                  SIMPLIFY = FALSE,
+                  USE.NAMES = FALSE)
+
+  names_ = lapply(objs, function(x) x$name)
+  series = setNames(series, names_)
+
+  if (length(series) == 1)
+    series[[1]]
+  else
+    series
+}
+
+.get_series = function(url) {
   res <- http_getter(url)
   if (res$status_code != 200) {
     stop("BCB API Request error, status code = ", res$status_code)
   }
-  # json_ <- httr::content(res, as = "text")
-  json_ <- http_gettext(res)
 
+  http_gettext(res)
+}
+
+create_series = function(json_, x, as) {
   if (as == 'text')
     return(json_)
 
   df_ <- jsonlite::fromJSON(json_)
-  names(df_) <- c('date', 'value')
 
-  df_ <- within(df_, {
+  names(df_) <- resolve_names(names(df_))
+
+  df_ = within(df_, {
     date <- as.Date(date, format = '%d/%m/%Y')
+    if (exists("end_date"))
+      end_date <- as.Date(end_date, format = '%d/%m/%Y')
     value <- as.numeric(value)
   })
 
-  name_ <- if (is.null(name)) code else name
+  name_ <- x$name
 
   switch (as,
-    'tibble' = {
-      df_ <- tibble::as_tibble(df_)
-      names(df_) <- c('date', name_)
-      df_
-    },
-    'data.frame' = {
-      names(df_) <- c('date', name_)
-      df_
-    },
-    'xts' = {
-      df_ <- xts::xts(df_$value, df_$date)
-      names(df_) <- name_
-      df_
-    },
-    'ts' = {
-      do.call(stats::ts, append(list(data = df_$value), ts_options))
-    }
+          'tibble' = {
+            df_ <- tibble::as_tibble(df_)
+            names(df_) <- set_series_name(names(df_), name_)
+            df_
+          },
+          'data.frame' = {
+            names(df_) <- set_series_name(names(df_), name_)
+            df_
+          },
+          'xts' = {
+            df_ <- xts::xts(df_$value, df_$date)
+            names(df_) <- name_
+            df_
+          },
+          'ts' = {
+            freq = if (is.null(x$info$frequency)) 'D' else x$info$frequency
+            freq_ = switch (freq,
+                           'A' = 1,
+                           'M' = 12,
+                           'D' = 366
+            )
+            start = switch (freq,
+              'A' = {
+                as.integer(format(df_$date[1], '%Y'))
+              },
+              'M' = {
+                c(
+                  as.integer(format(df_$date[1], '%Y')),
+                  as.integer(format(df_$date[1], '%m'))
+                )
+              },
+              'D' = {
+                c(
+                  as.integer(format(df_$date[1], '%Y')),
+                  as.integer(format(df_$date[1], '%j'))
+                )
+              }
+            )
+            stats::ts(df_$value, start = start, frequency = freq_)
+          }
   )
+}
 
-  # if (as == 'tibble') {
-  #   df_ <- tibble::as_tibble(df_)
-  #   names(df_) <- c('date', name_)
-  # } else if (as == 'data.frame') {
-  #   names(df_) <- c('date', name_)
-  # } else if (as == 'xts') {
-  #   df_ <- xts::xts(df_$value, df_$date)
-  #   names(df_) <- name_
-  # } else if (as == 'ts') {
-  #   df_ <- do.call(stats::ts, append(list(data = df_$value), ts_options))
-  # }
-  #
-  #   df_
+resolve_names = function(nx) {
+  ix = grep("^data$|^datafim$|^valor$", nx)
+  nm = sapply(ix, function(ix) {
+    nnx = sub("^data$", "date", nx[ix])
+    if (nnx != nx[ix]) return(nnx)
+    nnx = sub("^datafim$", "end_date", nx[ix])
+    if (nnx != nx[ix]) return(nnx)
+    nnx = sub("^valor$", "value", nx[ix])
+    if (nnx != nx[ix]) return(nnx)
+    nx[ix]
+  }, USE.NAMES = FALSE)
+  nx[ix] = nm
+  nx
+}
+
+set_series_name = function(nx, nm) {
+  ix = grep("^value$", nx)
+  nx[ix] = nm
+  nx
 }
--- tests/testthat/test-get_series.R
+++ tests/testthat/test-get_series.R
@@ -19,24 +19,29 @@ test_that("it should get one series as data.frame", {
 })
 
 test_that("it should name the series", {
-  x <- get_series(1, last = 10, name = "USD")
+  x <- get_series(c(USD = 1), last = 10)
   expect_equal(colnames(x), c("date", "USD"))
 })
 
 test_that("it should get series as xts", {
-  x <- get_series(1, last = 10, name = "USD", as = "xts")
+  x <- get_series(c(USD = 1), last = 10, as = "xts")
   expect_equal(colnames(x), "USD")
 })
 
 test_that("it should get series as ts", {
-  x <- get_series(433, start_date = as.Date("2017-01-01"), name = "IPCA", as = "ts", ts_options = list(start = c(2017, 1), frequency = 12))
+  x <- get_series(c(IPCA = 433), start_date = as.Date("2017-01-01"), as = "ts")
   expect_equal(frequency(x), 12)
   expect_is(x, "ts")
   expect_equal(start(x), c(2017, 1))
+
+  x <- get_series(27569, start_date = as.Date("2012-01-01"), as = "ts")
+  expect_equal(frequency(x), 1)
+  expect_is(x, "ts")
+  expect_equal(start(x), c(2012, 1))
 })
 
 test_that("it should get series within a date period", {
-  x <- get_series(1, start_date = "2017-03-01", end_date = "2017-03-29", name = "USD")
+  x <- get_series(c(USD = 1), start_date = "2017-03-01", end_date = "2017-03-29")
   expect_is(x$date, "Date")
   expect_true(x$date[1] == "2017-03-01")
   expect_true(x$date[dim(x)[1]] == "2017-03-29")
@@ -44,15 +49,25 @@ test_that("it should get series within a date period", {
 
 test_that("it should get series within a date period specifying only start_date", {
   start <- Sys.Date() - 10
-  x <- get_series(1, start_date = start, name = "USD")
+  x <- get_series(c(USD = 1), start_date = start)
   expect_is(x$date, "Date")
   expect_true(nrow(x) >= 6)
 })
 
 test_that("it should get series within a date period specifying only end_date", {
   end <- "2017-01-02"
-  x <- get_series(1, end_date = end, name = "USD")
+  x <- get_series(c(USD = 1), end_date = end)
   expect_is(x$date, "Date")
   expect_true(x$date[nrow(x)] == end)
 })
 
+test_that("it should get multiple series", {
+  start <- Sys.Date() - 10
+  x <- get_series(c(USD = 1, IBOVESPA = 7), start_date = start)
+  expect_equal(length(x), 2)
+  expect_equal(names(x), c("USD", "IBOVESPA"))
+
+  x <- get_series(c(USD = 1, 7), last = 5)
+  expect_equal(length(x), 2)
+  expect_equal(names(x), c("USD", "7"))
+})
